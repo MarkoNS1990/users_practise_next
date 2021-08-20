077024e18f02ae3ebe687dbddb52281133910be2
MarkoNS1990 <marczecevic@gmail.com>
2021-08-20 10:45:56 +0200

    Fix bug for loading 2nd page

--- pages/api/users/[userId].js
+++ pages/api/users/[userId].js
@@ -20,11 +20,16 @@ export default function handler(req, res) {
     const filePath = buildUsersPath();
     const data = extractUser(filePath);
     const name = req.body.name;
+    const image = req.body.image;
 
     const updatedUser = data.find((user) => user.id === parseInt(userId));
     const index = data.findIndex((user) => user.id === parseInt(userId));
 
-    data.splice(index, 1, { name: name, id: parseInt(userId) });
+    data.splice(index, 1, {
+      name: name,
+      id: parseInt(userId),
+      image: image,
+    });
     fs.writeFileSync(filePath, JSON.stringify(data));
 
     res.status(200).json({ message: "Success" });
